b3991605729df9f80fa0d95b3fe24281f78183ee
rifaulkhairunisa0731 <rifaul.khairunisa0731@student.unri.ac.id>
2025-09-20 00:59:18 +0700

    nambahin code

---
+++ latihankelas/src/main/java/com/example/ppbo/Main.java
@@ -0,0 +1,26 @@
+package com.example.ppbo;
+
+public class Main {
+    public static void main(String[] args) {
+        // Object disini ya
+        // 1. Buat object 1 dengan constructor default
+        Music Music1 = new Music();
+        Music1.tampilkanInfo();
+
+        // 2. Buat object 2 dengan constructor berparameter
+        Music Music2 = new Music("Thinking Out Loud", "Ed Sheeran", "Pop");
+        Music2.tampilkanInfo(true);
+
+        // 3. Buat object 3 dengan constructor berparameter
+        Music Music3 = new Music("The Scientist", "Coldplay", "Alternative Rock");
+        Music3.tampilkanInfo(false);
+    }
+ }
+
+
+
+
+        
+    
+
+        
