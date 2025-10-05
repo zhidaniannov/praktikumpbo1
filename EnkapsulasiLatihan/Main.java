bb8763d7fa58ce8e5fdbb764e7ae56c52cb24e23
selviaramadani0647 <selvia.ramadani0647@student.unri.ac.id>
2025-10-05 11:13:24 +0700

    Add files via upload

---
+++ EnkapsulasiLatihan/Main.java
@@ -0,0 +1,28 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Bank fakultas1 = new Bank("Bank Central Asia", "BCA");
+        Bank fakultas2 = new Bank("Bank Rakyat Indonesia", "BRI");
+        System.out.println("==================================");
+        fakultas1.tampilkanInfoBank();
+        System.out.println("==================================");
+
+        fakultas2.tampilkanInfoBank();
+        System.out.println("==================================");
+
+        fakultas1.jumlahBank();
+        System.out.println("==================================\n");
+        System.out.println("==================================");
+
+        Rekening r1 = new Rekening("Sarkawi", 20000);
+        Rekening r2 = new Rekening("Arifin", 50000);
+        r1.tampilkanInfoRekening();
+        System.out.println("==================================");
+        r2.tampilkanInfoRekening();
+
+        System.out.println("==================================");
+        System.out.println("Jumlah Rekening: " + Rekening.getjumlahRekening());
+        System.out.println("==================================");
+    }
+}
---
+++ EnkapsulasiLatihan/Rekening.java
@@ -0,0 +1,45 @@
+public class Rekening 
+{
+    private String namaPemilik;
+    private int saldo;
+    private static int jumlahRekening;
+
+    public Rekening(String namaPemilik, int saldo) 
+    {
+        this.namaPemilik = namaPemilik;
+        this.saldo = saldo;
+        jumlahRekening++;
+    }
+
+    public String getnamaPemilik() 
+    {
+        return namaPemilik;
+    }
+
+    public void setnamaPemilik(String namaPemilik) 
+    {
+        this.namaPemilik = namaPemilik;
+    }
+
+    public int getsaldo() 
+    {
+        return saldo;
+    }
+
+    public void setsaldo(int saldo) 
+    {
+        this.saldo = saldo;
+    }
+
+    public static int getjumlahRekening() 
+    {
+        return jumlahRekening;
+    }
+
+    public void tampilkanInfoRekening() 
+    {
+        System.out.println("Nama pemilik rekening: " + namaPemilik);
+        System.out.println("Jumlah saldo: " + saldo);
+    }
+    
+}
