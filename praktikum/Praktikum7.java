1366dabf7eedfd4b991f74219f6213516e972b39
muhammadradithia6081-dot <muhammad.radithia6081@student.unri.ac.id>
2025-10-11 15:33:20 +0700

    Add files via upload

---
+++ praktikum/Praktikum7.java
@@ -0,0 +1,20 @@
+public class Praktikum7 {
+
+    public static void main(String[] args) {
+        // BarangElektronik laptop = new BarangElektronik("Laptop", 15000000, 24);
+        // laptop.tampilkanInfo();
+        // laptop.tampilkanGaransi();
+        
+        Produk p1 = new Produk("Buku", 60000);
+        BarangElektronik p2 = new BarangElektronik("Mouse", 50000,6);
+
+        p1.tampilkanInfo();
+        System.out.println("Pajak produk umum: "+p1.hitungPajak());
+        System.out.println("Harga akhir Produk: "+p1.hitungHarga());
+        System.out.println("---------------");
+        p2.tampilkanInfo();
+        p2.tampilkanGaransi();
+        System.out.println("Pajak barang elektronik: "+p2.hitungPajak());
+        System.out.println("Harga akhir Produk: "+p2.hitungHarga());
+    }
+}
---
+++ praktikum/Produk.java
@@ -0,0 +1,25 @@
+public class Produk implements HargaAkhir {
+
+    protected String nama;
+    protected int harga;
+
+    public Produk(String nama, int harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Harga: " + harga);
+    }
+
+    public double hitungPajak(){
+        return harga * 0.05;
+    }
+
+    @Override
+    public double hitungHarga(){
+        return harga * 1.01;
+    }
+}
+
